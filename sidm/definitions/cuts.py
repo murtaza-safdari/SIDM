163099e6c844c4de31c7ab56b5fc4a71c52749f1
mariajose2398 <mariajose2398@gmail.com>
2026-01-20 14:05:58 +0000

    Defined  a selection for playing with data in the control region

--- sidm/definitions/cuts.py
+++ sidm/definitions/cuts.py
@@ -246,6 +246,7 @@ evt_cut_defs = {
     ">=2 LJs": lambda objs: ak.num(objs["ljs"]) >= 2,
     ">=1 egm_ljs": lambda objs: ak.num(objs["egm_ljs"]) >= 1,
     ">=1 mu_ljs": lambda objs: ak.num(objs["mu_ljs"]) >= 1,
+    "< 2 LJs": lambda objs: ak.num(objs["ljs"]) < 2,
     ">=2 matched As": lambda objs: ak.num(derived_objs["genAs_matched_lj"](objs, 0.2)) >= 2,
     # 4mu: leading two LJs are both mu-type
     "4mu": lambda objs: ak.count_nonzero(objs["ljs"][:, :2].muon_n >= 2, axis=-1) == 2,
