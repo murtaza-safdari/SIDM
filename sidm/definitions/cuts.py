b02052b40a54b914ccc9b3fc478334e8524458d5
mariajose2398 <mariajose2398@gmail.com>
2026-02-18 20:16:33 +0000

    defined a selection with 4j2btag

--- sidm/definitions/cuts.py
+++ sidm/definitions/cuts.py
@@ -290,5 +290,6 @@ evt_cut_defs = {
     "n_bjet == 2": lambda objs : ak.num(objs["bjets"]) == 2,
     "n_jet == 2": lambda objs : ak.num(objs["jets"]) == 2,
     "2j2btag": lambda objs : (ak.num(objs["jets"]) == 2) &  (ak.num(objs["bjets"]) == 2),
+    "4j2btag": lambda objs : (ak.num(objs["jets"]) == 4) &  (ak.num(objs["bjets"]) == 2),
     "dPhi(Mu_0, Mu_1) > 2.4": lambda objs: ak.fill_none(abs((ak.pad_none(objs["muons"], 2)[:,0].delta_phi(ak.pad_none(objs["muons"], 2)[:,1]))) > 2.4, False),
 }
