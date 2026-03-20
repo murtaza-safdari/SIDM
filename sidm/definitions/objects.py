5fcd96c7af39a3f9dfc2ba2a9f37d6fc03c3f672
mariajose2398 <mariajose2398@gmail.com>
2026-03-20 14:50:41 +0000

    commented out bjets

--- sidm/definitions/objects.py
+++ sidm/definitions/objects.py
@@ -68,7 +68,7 @@ preLj_objs["genAs_toE"]  = lambda evts: toPid(preLj_objs["genAs"](evts), 11)
 preLj_objs["rho_PFIso"]  = lambda evts: evts.fixedGridRhoFastjetAll
 preLj_objs["jets"]       = lambda evts: evts.Jet
 preLj_objs["flags"]       = lambda evts: evts.Flag
-preLj_objs["bjets"] = lambda evts: evts.Jet[evts.Jet.btagDeepFlavB >=  0.7100]
+#preLj_objs["bjets"] = lambda evts: evts.Jet[evts.Jet.btagDeepFlavB >=  0.7100]
 # define objects whose that will be added to objs by the sidm_processor after LJs are clustered
 # and LJ cuts are applied. postLj_obj cuts can be applied to these
 postLj_objs = {}
