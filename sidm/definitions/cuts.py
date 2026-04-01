446dff0c0169a2df4b84b1b926053bf2fe55c0c6
mariajose2398 <mariajose2398@gmail.com>
2026-04-01 20:54:33 +0000

    added another cosmic veto with combination muons

--- sidm/definitions/cuts.py
+++ sidm/definitions/cuts.py
@@ -311,6 +311,8 @@ evt_cut_defs = {
     "inv(lj1, lj2) <= 100": lambda objs : (objs["ljs"][:,:2].sum().mass) <= 100,
     "cosA_muons > -0.95":  lambda objs : cosA_cut(objs["muons"]),
     "cosA_dsaMuons > -0.95":  lambda objs : cosA_cut(objs["dsaMuons"]),
+    "cosmic_mu_pair <= 6":lambda objs : cosA_pair_cut(objs["muons"]),
+    "cosmic_dsaMu_pair <= 6":lambda objs : cosA_pair_cut(objs["dsaMuons"]),
     "pass two missing triggers": lambda objs: (
     (
         objs["hlt"].DoubleL2Mu23NoVtx_2Cha_NoL2Matched
