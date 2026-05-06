e4aee2ccb457d300003a83d8d19e3a77a9e2ea45
mariajose2398 <mariajose2398@gmail.com>
2026-05-06 18:48:23 +0000

    added number of parellel pairs for muons inside mu ljs

--- sidm/definitions/hists.py
+++ sidm/definitions/hists.py
@@ -750,6 +750,20 @@ hist_defs = {
                    lambda objs, mask: ak.sum(cosAlpha(objs["dsaMuons"]) <= -0.95, axis =1)),
         ],
     ),
+    "N_back_to_back_mu_lj_dsa": h.Histogram(
+        [
+            h.Axis(hist.axis.Regular(10, 0, 10, name="N_back_to_back_mu_lj_dsa",
+                                     label=r"N cos ($\mu$ LJ DSA $\mu$,$\mu$ LJ DSA $\mu$) <= -0.95 "),
+                   lambda objs, mask: ak.sum(cosAlpha(objs["mu_ljs"].dsaMuons) <= -0.95, axis =1)),
+        ],
+    ),
+    "N_back_to_back_mu_lj_pf": h.Histogram(
+        [
+            h.Axis(hist.axis.Regular(10, 0, 10, name="N_back_to_back_mu_lj_pf",
+                                     label=r"N cos ($\mu$ LJ PF $\mu$,$\mu$ LJ PF $\mu$) <= -0.95 "),
+                   lambda objs, mask: ak.sum(cosAlpha(objs["mu_ljs"].pfMuons) <= -0.95, axis =1)),
+        ],
+    ),
     "N_back_to_back_pf": h.Histogram(
         [
             h.Axis(hist.axis.Regular(10, 0, 10, name="N_back_to_back_pf",
@@ -769,6 +783,20 @@ hist_defs = {
             h.Axis(hist.axis.Regular(10, 0, 10, name="N_back_to_back_pf",
                                      label=r"N cos (PF $\mu$, PF $\mu$) >= 0.95 "),
                    lambda objs, mask: ak.sum(cosAlpha(objs["muons"]) >= 0.95, axis =1)),
+        ],
+    ),
+    "N_parallel_mu_lj_dsa": h.Histogram(
+        [
+            h.Axis(hist.axis.Regular(10, 0, 10, name="N_parallel_mu_lj_dsa",
+                                     label=r"N cos ($\mu$ LJ DSA $\mu$,$\mu$ LJ DSA $\mu$) >= 0.95 "),
+                   lambda objs, mask: ak.sum(cosAlpha(objs["mu_ljs"].dsaMuons) >= 0.95, axis =1)),
+        ],
+    ),
+    "N_parallel_mu_lj_pf": h.Histogram(
+        [
+            h.Axis(hist.axis.Regular(10, 0, 10, name="N_parallel_mu_lj_pf",
+                                     label=r"N cos ($\mu$ LJ PF $\mu$,$\mu$ LJ PF $\mu$) >= 0.95 "),
+                   lambda objs, mask: ak.sum(cosAlpha(objs["mu_ljs"].pfMuons) >= 0.95, axis =1)),
         ],
     ),
     # lj
