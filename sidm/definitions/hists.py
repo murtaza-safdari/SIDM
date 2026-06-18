89e6dca1ec14aafb5001fc8561b19d362ef6973f
Maria Jose <mariajose2398@gmail.com>
2026-06-18 11:50:00 -0500

    added histograms that plots the diff of matched lj index of dsa pairs

--- sidm/definitions/hists.py
+++ sidm/definitions/hists.py
@@ -13,7 +13,7 @@ import hist
 import awkward as ak
 # local
 from sidm.tools import histogram as h
-from sidm.tools.utilities import dR, lxy, lxyz, lxyz_proper, betagamma, matched, dxy, lepton_dxy_resolution, cosAlpha, cosAlpha_lj
+from sidm.tools.utilities import dR, lxy, lxyz, lxyz_proper, betagamma, matched, dxy, lepton_dxy_resolution, cosAlpha, cosAlpha_lj, nearest_lj_index
 from sidm.definitions.objects import derived_objs
 # always reload local modules to pick up changes during development
 importlib.reload(h)
@@ -1128,6 +1128,27 @@ hist_defs = {
                                      label=r"back to back DSA-DSA 3D Distance "),
                   lambda objs, mask: (lambda v1, v2: np.sqrt((v1.vx - v2.vx)**2 +(v1.vz - v2.vz)**2))
                                                         (*ak.unzip(derived_objs["parallel_dsa_pairs"](objs))))
+        ],
+    ),
+    "dsa_pair_nearest_lj_index_diff": h.Histogram(
+        [
+            h.Axis(hist.axis.Regular(15, 0, 15, name="dsa_pair_nearest_lj_index_diff",
+                                     label=r" DSA-DSA pair abs( nearest LJ index) "),
+                  lambda objs, mask: (lambda v1, v2, objs: abs(nearest_lj_index(v1, objs)  - nearest_lj_index(v2, objs)))(*ak.unzip(objs["dsaMuonPairs"]), objs["ljs"])),
+        ],
+    ),
+    "back_to_back_dsa_pair_nearest_lj_index_diff": h.Histogram(
+        [
+            h.Axis(hist.axis.Regular(15, 0, 15, name="back_to_back_dsa_pair_nearest_lj_index_diff",
+                                     label=r" back to back DSA-DSA pair abs(nearest LJ index difference) "),
+                  lambda objs, mask: (lambda v1, v2, objs: abs(nearest_lj_index(v1, objs)  - nearest_lj_index(v2, objs)))(*ak.unzip(derived_objs["back_to_back_dsa_pairs"](objs)), objs["ljs"])),
+        ],
+    ),
+    "parallel_dsa_pair_nearest_lj_index_diff": h.Histogram(
+        [
+            h.Axis(hist.axis.Regular(15, 0, 15, name="parallel_dsa_pair_nearest_lj_index_diff",
+                                     label=r"parallel DSA-DSA pair abs(nearest LJ index difference) "),
+                  lambda objs, mask: (lambda v1, v2, objs: abs(nearest_lj_index(v1, objs)  - nearest_lj_index(v2, objs)))(*ak.unzip(derived_objs["parallel_dsa_pairs"](objs)), objs["ljs"])),
         ],
     ),
     "dsa_pair_vx_diff": h.Histogram(
